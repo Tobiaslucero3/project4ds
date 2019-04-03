7bd84ce615cf0caf8c927784825e529a01cb66b0
Tobias Lucero <tobylucero1@ou.edu>
2019-04-02 17:08:16 -0700

    Created tree class

--- main.cpp
+++ main.cpp
@@ -2,6 +2,30 @@
 
 using namespace std;
 
+template <class DT>
+class Tree{
+    protected:
+        int* _parentArray[];
+        int root;
+    public:
+        Tree();
+        Tree(int numNodes);
+        Tree(Tree tree);
+        ~Tree();
+        void LCA(int node, int otherNode);
+        int& Parent(int i);
+        int[] Children(int i);
+        int[] Siblings(int i);
+        void setRoot(int& rootNode);
+        void setParent(int node, int parent);
+        int nodesAtLevel(int i);
+        int Level(int i);
+        int height();
+        void Preorder();
+
+
+};
+
 int main()
 {
     cout << "Hello world!" << endl;
